f5123859328440b8cac1ff99b92af4f1aced11c9
Seni Oluwunmi <olaseni.oluwunmi@laitech.com>
2018-12-12 15:12:14 +0100

    Implemented: reports and charts
    - Integrated Fusion charts
    - Wrote another template tag to insert charts into reports

--- django-context/src/dps_main/templatetags/dps_tags.py
+++ django-context/src/dps_main/templatetags/dps_tags.py
@@ -1,4 +1,9 @@
+from random import randint
+
 from django import template
+from collections import OrderedDict
+
+from dps_main.utilities.fusioncharts import FusionCharts
 
 from dps_main.utilities.actions import ActionHelper
 
@@ -11,3 +16,43 @@ def action_helper_ping(context):
     Pings the action helper
     """
     return ActionHelper(context.get('user')).ping()
+
+
+@register.inclusion_tag(takes_context=True, filename='dps_main/tags/fusion_bar_chart.html')
+def fusion_bar_chart_cause(context, report, caption='Title', x_axis='X Axis', y_axis='Y Axis', number_prefix='',
+                           number_suffix=''):
+    # Chart data is passed here in key-value pairs.
+    data_source = OrderedDict()
+
+    # Dict contains key-value pairs of data for chart attribute
+    config = {"caption": caption,
+              "xAxisName": x_axis,
+              "yAxisName": y_axis,
+              "numberPrefix": number_prefix,
+              "numberSuffix": number_suffix,
+              "theme": "fusion"}
+
+    # report should be an ordereddict containing key-value pairs of data
+    report = report or OrderedDict()
+
+    data_source["chart"] = config
+    data_source["data"] = []
+
+    # Convert the data in the `chartData`array into a format that can be consumed by FusionCharts.
+    # The data for the chart should be in an array wherein each element of the array
+    # is a JSON object# having the `label` and `value` as keys.
+
+    # Iterate through the data in `chartData` and insert into the `dataSource['data']` list.
+    for key, value in report.items():
+        data = {"label": key, "value": value}
+        data_source["data"].append(data)
+
+    html_id = F'fusion-{randint(11111111, 999999999)}-chart'
+    html_div_container = F'{html_id}-container'
+
+    # Create an object for the chart using the FusionCharts constructor
+    chart = FusionCharts("bar3d", html_id, "80%", "500", html_div_container, "json", data_source)
+
+    return {'output': chart.render(),
+            'html_id': html_id,
+            'html_container': html_div_container}
--- django-context/src/dps_main/utilities/admin.py
+++ django-context/src/dps_main/utilities/admin.py
@@ -1,14 +1,17 @@
+from collections import OrderedDict
+
 from django.contrib import admin
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 from adminplus.sites import AdminSitePlus
+
+from .reports import top_causes_by_amount, top_causes_by_promises
 
 __all__ = ['register_admin_views']
 
 
 class AdminSite(AdminSitePlus):
-    index_template = 'admin/index.html'
+    index_template = 'dps_main/admin/index.html'
 
 
 def register_admin_views():
@@ -16,22 +19,29 @@ def register_admin_views():
     Registers admin customizations
     """
 
+    def query_to_dict(q, k, v):
+        """
+        Consume a queryset into a dict
+        """
+        o = OrderedDict()
+        for item in q:
+            o['%s/%s' % (getattr(item, 'id'), getattr(item, k))] = getattr(item, v)
+        return o
+
     @admin.site.register_view('reports/causes/amount', name='Top 5 causes by amount')
     def reports_cause_amount(request):
         """
         A view to provide reports
         """
-        # Fanciness.
-        return render_to_response('admin/reports/causes-amount.html',
-                                  {'title': 'Top causes by amount'},
-                                  RequestContext(request, {}))
+        return render(request, 'dps_main/admin/reports/causes-amount.html',
+                      {'title': 'Top causes by amount',
+                       'report': query_to_dict(top_causes_by_amount(), 'title', 'sum')})
 
     @admin.site.register_view('reports/causes/promises', name='Top 5 causes by promises')
     def reports_causes_promises(request):
         """
         A view to provide reports
         """
-        # Fanciness.
-        return render_to_response('admin/reports/causes-promises.html',
-                                  {'title': 'Top causes by promises'},
-                                  RequestContext(request, {}))
+        return render(request, 'dps_main/admin/reports/causes-promises.html',
+                      {'title': 'Top causes by promises',
+                       'report': query_to_dict(top_causes_by_promises(), 'title', 'count')})
